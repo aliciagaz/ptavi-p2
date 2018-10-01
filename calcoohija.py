84537f2e3d05f18ddfbc58ade24ba96580fce2e9
Alicia Gordo <43533576+aliciagaz@users.noreply.github.com>
2018-10-01 14:54:01 +0200

    Segundo ejercicio

--- calcoohija.py
+++ calcoohija.py
@@ -20,19 +20,21 @@ if __name__ == "__main__":
     try:
         operando1 = int(sys.argv[1])
         operando2 = int(sys.argv[3])
+
+        if sys.argv[2] == "suma":
+            result = (calchija.plus(operando1, operando2))
+        elif sys.argv[2] == "resta":
+            result = (calchija.minus(operando1, operando2))
+        elif sys.argv[2] == "multiplica":
+            result = (calchija.mult(operando1, operando2))
+        elif sys.argv[2] == "divide":
+            result = (calchija.div(operando1, operando2))
+        else:
+            sys.exit("Error: La operación sólo puede ser suma"
+                     + "resta, multiplica o divide")
+
     except ValueError:
         sys.exit("Error: Non numerical parameters")
-
-    if sys.argv[2] == "suma":
-        result = (calchija.plus(operando1, operando2))
-    elif sys.argv[2] == "resta":
-        result = (calchija.minus(operando1, operando2))
-    elif sys.argv[2] == "multiplica":
-        result = (calchija.mult(operando1, operando2))
-    elif sys.argv[2] == "divide":
-        result = (calchija.div(operando1, operando2))
-    else:
-        sys.exit("Error: La operación sólo puede ser suma"
-                 + "resta, multiplica o divide")
-
+    except ZeroDivisionError:
+        sys.exit("Division by zero is not allowed")
     print("El resultado es " + str(result))
